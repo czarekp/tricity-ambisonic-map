0767030d2b3d42d267cf5ef1d2993619c36fe955
czarekp <cezarypiotrpietrzak@gmail.com>
2020-08-29 18:13:05 +0200

    Add VueLayers library

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,30 @@
 <template>
-  <v-container></v-container>
+  <v-container fluid style="width: 100%; height: 100vh" class="pa-0 ma-0">
+    <vl-map
+      :load-tiles-while-animating="true"
+      :load-tiles-while-interacting="true"
+      style="width: 100%; height: 100%"
+    >
+      <vl-view
+        :zoom.sync="zoom"
+        :center.sync="center"
+        :rotation.sync="rotation"
+      ></vl-view>
+
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "Map",
-  data: () => ({})
+  data: () => ({
+    zoom: 2,
+    center: [0, 0],
+    rotation: 0
+  })
 };
 </script>
