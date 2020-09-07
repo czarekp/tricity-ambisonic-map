2b1fe824d6c8d05d79ed04a10f32df148565a658
czarekp <cezarypiotrpietrzak@gmail.com>
2020-09-07 04:39:40 +0200

    Update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,26 @@
-import merge from "webpack-merge";
-
 module.exports = {
   transpileDependencies: ["vuetify"],
-  chainWebpack: config => {
-    config.module
-      .rule("vue")
-      .use("vue-loader")
-      .tap(options =>
-        merge(options, {
-          transformAssetUrls: {
-            video: ["src", "poster"],
-            source: "src",
-            img: "src",
-            image: "xlink:href",
-            "vl-style-icon": "src"
-          }
-        })
-      );
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: [
+            {
+              loader: "vue-loader",
+              options: {
+                transformAssetUrls: {
+                  video: ["src", "poster"],
+                  source: "src",
+                  img: "src",
+                  image: "xlink:href",
+                  "vl-style-icon": "src"
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
   }
 };
