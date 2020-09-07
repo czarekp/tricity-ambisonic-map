17e827f97dfb826d0f286d294b83512c0748359a
czarekp <cezarypiotrpietrzak@gmail.com>
2020-09-07 05:03:59 +0200

    Remove configureWebpack property

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,20 @@ const merge = require("webpack-merge");
 
 module.exports = {
   transpileDependencies: ["vuetify"],
-  configureWebpack: {
-    chainWebpack: config => {
-      config.module
-        .rule("vue")
-        .use("vue-loader")
-        .tap(options =>
-          merge(options, {
-            transformAssetUrls: {
-              video: ["src", "poster"],
-              source: "src",
-              img: "src",
-              image: "xlink:href",
-              "vl-style-icon": "src"
-            }
-          })
-        );
-    }
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap(options =>
+        merge(options, {
+          transformAssetUrls: {
+            video: ["src", "poster"],
+            source: "src",
+            img: "src",
+            image: "xlink:href",
+            "vl-style-icon": "src"
+          }
+        })
+      );
   }
 };
